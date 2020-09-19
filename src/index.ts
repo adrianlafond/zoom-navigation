5ec379cd96ec2ab44cf4207263030ffa9e82440e
Adrian Lafond <mail@adrianlafond.com>
2020-09-19 18:32:02 -0400

    implement scss

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { ZoomNavigation } from './zoom-navigation';
 
+import './style/index.scss';
+
 window.addEventListener('load', () => {
   const zoom = new ZoomNavigation();
 });
--- src/zoom-navigation.ts
+++ src/zoom-navigation.ts
@@ -1,6 +1,7 @@
 export class ZoomNavigation {
   constructor() {
-    const el: HTMLDivElement = document.createElement('div');
+    const el: HTMLDivElement = document.createElement('p');
+    el.className = 'zoom-navigation';
     el.textContent = 'ZOOM';
     document.body.appendChild(el);
   }
